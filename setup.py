5cfa381f7d5be4b9f97b34fc22df66aa71056a08
ostr00000 <ostr00000@gmail.com>
2017-05-08 16:26:58 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='1',
+      description='compute complexity funtion',
+      url='https://github.com/AGHPythonCourse2017/zad2-ostr00000',
+      author='ostr00000',
+      author_email='ostr00000@gmail.com',
+      license='MIT',
+      packages=['comp'],
+      zip_safe=False)
