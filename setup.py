2271c638a6e0e15a65801d630863b963a49cd77b
ostr00000 <ostr00000@gmail.com>
2018-07-20 16:41:33 +0200

    github address change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='funniest',
       version='1',
       description='compute complexity funtion',
-      url='https://github.com/AGHPythonCourse2017/zad2-ostr00000',
+      url='https://github.com/ostr00000/python-coure-zad2-computing-complexity',
       author='ostr00000',
       author_email='ostr00000@gmail.com',
       license='MIT',
